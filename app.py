b774183263d591a88af522155ddcf41762e4e174
busyfingers <n.malmqvist@gmail.com>
2019-12-22 21:15:56 +0100

    Viewport meta tag for responsive layoout

--- app.py
+++ app.py
@@ -32,7 +32,9 @@ def fetchData(date_from, date_to):
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+])
 
 colors = {
     'background': '#EEEEEE',
